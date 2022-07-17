a4e894d36db6cb45b3dcd23c922c43c1b2257d61
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-07-17 18:56:49 +0530

    first commit

---
+++ Shiyam-CoreJava/src/main/java/JAVA/Shiyam_CoreJava/looping.java
@@ -0,0 +1,14 @@
+package JAVA.Shiyam_CoreJava;
+
+public class looping {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			int a=	10;
+			System.out.println(a);
+			long h=797866985l;
+			System.out.println(h);
+			
+	}
+
+}
