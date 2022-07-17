e96fbc22f95a96401347c9e4be5618083f4d5a04
Manojkumar886 <manojgeetha12.10.2001@gmail.com>
2022-07-17 19:01:14 +0530

    Looping

--- Shiyam-CoreJava/src/main/java/JAVA/Shiyam_CoreJava/looping.java
+++ Shiyam-CoreJava/src/main/java/JAVA/Shiyam_CoreJava/looping.java
@@ -4,11 +4,23 @@ public class looping {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			int a=	10;
-			System.out.println(a);
-			long h=797866985l;
-			System.out.println(h);
-			
+			for(int a=1;a<10;a++)
+			{
+				System.out.println(a);
+			}
+			int atm=1;
+			while(atm<5)
+			{
+				System.out.println("money withdraw");
+				atm++;
+			}
+			int counter=0;
+			do
+			{
+				System.out.println("encounter yegambaram -maruthamalai movie");
+				counter++;
+			}
+			while(counter<=10);
 	}
 
 }
